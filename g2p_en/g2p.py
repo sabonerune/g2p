0b282debb4bdd605b0aeaf90cb4f0dfaf2f88903
sabonerune <102559104+sabonerune@users.noreply.github.com>
2025-02-27 01:46:27 +0900

    FIX: fix minor bug

--- g2p_en/g2p.py
+++ g2p_en/g2p.py
@@ -180,7 +180,7 @@ class G2p:
         text = ''.join(char for char in unicodedata.normalize('NFD', text)
                        if unicodedata.category(char) != 'Mn')  # Strip accents
         text = text.lower()
-        text = re.sub("[^ a-z'.,?!\-]", "", text)
+        text = re.sub("[^ a-z'.,?!\\-]", "", text)
         text = text.replace("i.e.", "that is")
         text = text.replace("e.g.", "for example")
 
